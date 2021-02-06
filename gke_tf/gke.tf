4ce9fd2a4e87d2e9bafb542792750d248ba78066
Pavan Kumar <34813177+pavan-kumar-99@users.noreply.github.com>
2021-02-06 20:15:49 +0530

    Update gke.tf

--- gke_tf/gke.tf
+++ gke_tf/gke.tf
@@ -41,6 +41,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
       "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/ndev.clouddns.readwrite",
     ]
   }
 }
